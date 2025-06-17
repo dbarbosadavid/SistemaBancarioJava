fd194ede8eba787684e40272b4a4882fd94da978
aluno <aluno@LAB>
2025-06-17 20:23:06 -0300

    https://github.com/dbarbosadavid/SistemaBancarioJava.git

---
+++ src/sistemalogin/ValidadorUser.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemalogin;
+
+/**
+ *
+ * @author aluno
+ */
+public class ValidadorUser {
+    public boolean validarUser() {
+        System.out.println("Validando usuario...");
+        System.out.println("Usuario autenticado.");
+        return true; 
+    }
+}
